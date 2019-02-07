3f4ba5bcafe8fa53e878bdbaeeb06ba71f18dc15
AGE <age.esquivel@gmail.com>
2019-02-07 11:34:51 -0600

    Updated spellchecker function/API. I ended up spiltting the string into an array, running it through a loop and selecting our chemWords. This will get us where we need to be unless user really messes up the spelling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,42 +23,87 @@ firebase.initializeApp(config);
 //dataBase reference
 let dbRef = firebase.database();//variable needed to create 'folder' for each med selection
 
+
+
+
+//#############################################################################################
+// Start of API/AJAX queries
+//#############################################################################################
+//Spellcheck API
+//TODO: Run the user input from through this function 
 function spellChecker(med) {
+  //Empty variable will be use to store array
+  var spell;
+
+  //Query for spell checker API.
   let spellQuery = "https://cors-anywhere.herokuapp.com/https://chemspell.nlm.nih.gov/spell/restspell/restSpell/getQuery4JSON?query=" + med
+
+  //AJAX call for API
   $.ajax({
     url: spellQuery,
     method: "GET"
   }).then(function (response) {
-    console.log(response)
-    var spell = response;
-    console.log(JSON.stringify(spell));
-  })
-}
 
-// AJAX queries
-function userSelection(med) {
+    console.log("This is the response form chemSpell: ", response)//<---checking info from API
+
+    //TODO: Turn our response into an array and split it from the '"'
+    spell = response.split('"');
+
+    //TODO: review all of our elements with a loop
+    for (let i = 0; i < spell.length; i++) {
+      //Capture our elements
+      const el = spell[i];
+      //TODO: This if will check first character of the elements in the array and check against user input first letter.
+      if (el.charAt(0) == med.charAt(0).toUpperCase()) {
+
+        console.log("Elements from array that match the chacrater: ", el);
+
+        //TODO: Will need to pass this result  into a button or list creater function 
+        //to populate DOM with correct spelling for user to select 
+
+      };
+    };
+  });
+};
+
+//  FDA API 
+//TODO: When user selects the correct spelling, i should send the value into the FDA functoin
+function FDA(med) {
+  //Query for FDA API.
   let FDAQuery = "https://api.fda.gov/drug/label.json?search=" + med; //Will need a variable in place
-  //let FDAQuery = "https://api.fda.gov/drug/label.json?search=prozac"; //Will need a variable in place
-  medDB = dbRef.ref('med/' + med);//variable needed to create 'folder' for each med selection
+
+
+
 
   $.ajax({
     url: FDAQuery,
     method: "GET"
   }).then(function (response) {
 
-    console.log("This is the full response: ", response);
-    console.log("This is the meta disclaimer for the API: ", response.meta.disclaimer);
-    console.log("The results are in an array: ", response.results[0]); //<--response.results[0] should be assigned to variable
-    console.log("This is the results indexed for adverse_reactions which also come in an array: ", response.results[0].adverse_reactions);
-    console.log("These are the results for adverse_reactions index 0:", response.results[0].adverse_reactions[0]);
+    console.log("This is the full response from FDA: ", response);//<---checking info from API
 
 
     //var medDB = dbRef.push()
     medDB.set({
       adverse_reactions: response.results[0].adverse_reactions,
       boxed_warning: response.results[0].boxed_warning
-    })
+    });
 
   });
 
-}
+};
+//#############################################################################################
+// End of API/AJAX queries
+//#############################################################################################
+
+
+//Will need to creat an addBtn function to pass user selection into the data base below
+//variable needed to create 'folder' for each med selection
+medDB = dbRef.ref('med/' + med);
+//Save data to folder with .set()
+medDB.set({
+  //KEY:VALUE,
+  //KEY:VALUE,
+  //KEY:VALUE
+})
+
